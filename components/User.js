2e402ac9193373fadbc05f7a804c3eeb68481cb2
Viktor200719821974 <viktorvanyatanya@gmail.com>
2021-09-15 23:05:05 +0200

    lesson_1 read

--- components/User.js
+++ components/User.js
@@ -4,11 +4,27 @@ import {View, Text, Button, TouchaleOpacity} from 'react-native';
 
 const User = ({item}) =>{
 return (
-<View>
-<Text> {item.name}</Text>
+<View style={[styles.userBox, styles.margins, styles.sizes]}>
+<Text style={[styles.align]}> {item.name}</Text>
 </View>
 );
 };
 export default User;
 
-let styles = StyleSheet.create({});
+let styles = StyleSheet.create({
+userBox: {
+        flex: 1,
+        backgroundColor: 'silver'
+
+},
+margins:{
+        marginBottom: 3
+},
+sizes: {
+         width: '70%',
+         height: 200
+},
+align: {
+         textAlign: 'center'
+}
+});
--- components/Users.js
+++ components/Users.js
@@ -1,20 +1,20 @@
 import React, {useEffect, useState} from 'react';
 import {FlatList, StyleSheet} from 'react-native';
 import {View, Text, Button, TouchaleOpacity} from 'react-native';
-import {getUsers} from '../services/api.service';
-import User from './User';
+import {getUsers} from "../services/api.service";
+import User from "./User";
 
 const Users = () =>{
 let [users, setUsers] = useState();
 useEffect(()=>{
-getUsers().then(value => setUsers([...value]));
+fetch('https://jsonplaceholder.typicode.com/users')
+.then(value => value.json())
+.then(value => setUsers([...value]));
 }, []);
 return (
 <View>
-<FlatList data={users} renderItem = {({item}) => {
-return
-<User item={item}/>
-}}
+<FlatList data={users} renderItem = {({item}) => <User item={item}/>}
+keyExtractor={item => '' + item.id}
 />
 </View>
 );
